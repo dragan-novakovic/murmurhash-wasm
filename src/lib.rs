8e5c43b9daa5188b2eda700b4793143c38028633
Dragan <d.novakovic1810@yahoo.com>
2024-01-21 20:52:48 +0100

    fix padding

--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,15 @@ use fastmurmur3::murmur3_x64_128;
 pub fn hash(key: &str) -> String {
     let bytes = key.as_bytes();
     let hash_result = murmur3_x64_128(bytes, 0);
-    return String::from(format!("{:x}", hash_result.to_be()));
+    let mut hex_result = format!("{:x}", hash_result.to_be());
+
+    if hex_result.len() != 32 {
+        let mut padding = String::new();
+        for _ in 0..(32 - hex_result.len()) {
+            padding.push('0');
+        }
+        hex_result = format!("{}{}", padding, hex_result);
+    }
+
+    hex_result
 }
